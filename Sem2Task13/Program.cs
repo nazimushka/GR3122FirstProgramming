06615745a7a8ebbe386c5f21d33d3e55ddfaf632
Gasanov Nazim <nazimushka@gmail.com>
2022-09-07 22:46:14 +0300

    Домашнее задание по второму семинару

---
+++ Sem2Task13/Program.cs
@@ -0,0 +1,36 @@
+﻿// # 13 Напишите программу, которая выводит третью
+// цифру заданного числа или сообщает,
+// что третьей цифры нет.
+
+// 10-ю решаем без звездочки, а 13-ю со *
+
+// 1. A=Console.Readline();
+// 2. if (A length > = 3)
+// {
+// 3. [charr] Arr. = A To CharArray
+//      [0][1][2]
+// 4. Consol. WriteLine(Arr[2])
+// }
+// else
+// {
+// Consol.WriteLine ("Такого числа нет");
+// }
+
+void Gasanov()
+{
+    Console.WriteLine("Введите число: ");
+
+    string inputLine = Console.ReadLine() ?? "";
+
+    if (inputLine.Length >= 3)
+    {
+        char[] charArray = inputLine.ToString().ToCharArray();
+        Console.WriteLine(charArray[2]);
+    }
+    else
+    {
+        Console.WriteLine("Третьей цифры нет");
+    }
+}
+
+Gasanov();
---
+++ Sem2Task15/Program.cs
@@ -0,0 +1,41 @@
+﻿// #15 Напишите программу, которая принимает на вход цифру,
+// обозначающую день недели,
+// и проверяет, является ли этот день выходным.
+
+// 1. A=Console.Readline();
+// 2. D=int.Parse (A);
+// 3.if D = = 6 || D = = 7
+// Console.WriteLine("выходной день")
+// {
+//     else
+// }
+// Console.WriteLine("будний")
+
+// * второй вариант
+// Dictionary
+void Gasanov()
+{
+    Console.WriteLine("Введите цифру: ");
+
+    string inputLine = Console.ReadLine() ?? "";
+
+    int DayOfWeek = int.Parse(inputLine);
+
+    if (DayOfWeek > 7 || DayOfWeek < 1)
+    {
+        Console.WriteLine("Превышен лимит дней! В неделе - только 7");
+    }
+    else
+    {
+        if (DayOfWeek == 6 || DayOfWeek == 7)
+        {
+            Console.WriteLine("Выходной день");
+        }
+        else
+        {
+            Console.WriteLine("Будний день");
+        }
+    }
+}
+
+Gasanov();
