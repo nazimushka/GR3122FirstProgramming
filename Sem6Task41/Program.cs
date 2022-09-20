4d046c896deaaf9c9d6fe8f8751f620f863d1312
Gasanov Nazim <nazimushka@gmail.com>
2022-09-21 01:18:47 +0300

    Результаты домашней работы №5

---
+++ Sem6Task41/Program.cs
@@ -0,0 +1,41 @@
+﻿// # 41 Пользователь вводит с клавиатуры M чисел.
+// Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// -1, -7, 567, 89, 223-> 3
+// * Пользователь вводит число нажатий, затем программа следит за нажатиями и
+// выдает сколько чисел больше 0 было введено.
+
+// Метод считывания данных пользователя
+int ReadData(string line)
+{
+    // Выводим сообщение
+    Console.Write(line);
+    // Считываем число
+    int number = int.Parse(Console.ReadLine() ?? "0");
+    // Возвращаем значение
+    return number;
+}
+
+// Метод, принимает строку, выводит в консоль
+void PrintResult(string line)
+{
+    Console.WriteLine(line);
+}
+
+// Метод, который выдаст кол-во чисел больше 0
+int Count(int numOfDigits)
+{
+    int res = 0;
+    for (int i = 0; i < numOfDigits; i++)
+    {
+        int number = ReadData("Введите число: ");
+        if (number > 0)
+            res++;
+    }
+    return res;
+}
+
+int numOfDigits = ReadData("Введите количество чисел: ");
+int amount = Count(numOfDigits);
+
+PrintResult("Сумма цифр больше 0: " + amount);
---
+++ Sem6Task43/Program.cs
@@ -0,0 +1,41 @@
+﻿// # 43 Напишите программу, которая найдёт точку пересечения двух прямых, заданных
+// уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются
+// пользователем.
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; 5,5)
+// * Найдите площадь треугольника образованного пересечением 3 прямых
+
+// Метод считывания данных пользователя
+int ReadData(string line)
+{
+    // Выводим сообщение
+    Console.Write(line);
+    // Считываем число
+    int number = int.Parse(Console.ReadLine() ?? "0");
+    // Возвращаем значение
+    return number;
+}
+
+// Метод, принимает строку, выводит в консоль
+void PrintResult(string line)
+{
+    Console.WriteLine(line);
+}
+
+// Метод, который находит точку пересечения
+int[] PointFind(int k1, int k2, int b1, int b2)
+{
+    int[] outarray = new int[2];
+    int x = (b2 - b1) / (k1 - k2);
+    int y = k1 * x + b1;
+    outarray[0] = x;
+    outarray[1] = y;
+
+    return outarray;
+}
+
+int k1 = ReadData("Введите число k1: ");
+int k2 = ReadData("Введите число k2: ");
+int b1 = ReadData("Введите число b1: ");
+int b2 = ReadData("Введите число b2: ");
+int[] pointCoordin = PointFind(k1, k2, b1, b2);
+PrintResult("Координаты точки пересечения 2-х прямых: " + (pointCoordin[0], pointCoordin[1]));
