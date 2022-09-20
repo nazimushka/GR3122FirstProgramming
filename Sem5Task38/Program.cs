21f200489eaff72ba230663dd2e960f68d653403
Gasanov Nazim <nazimushka@gmail.com>
2022-09-21 01:27:25 +0300

    Результаты домашней работы №5 (помог преподаватель)

--- Sem5Task38/Program.cs
+++ Sem5Task38/Program.cs
@@ -8,4 +8,53 @@
 // использовать заполнение массива целыми числами.
 
 
-Я НЕ ПОНЯЛ КАК РЕШАТЬ, ПОЭТОМУ ЭТУ ДОМАШКУ Я СДАМ БЕЗ ЭТОЙ ЗАДАЧИ
+// Печать результата
+void PrintResult(string line)
+{
+    Console.WriteLine(line);
+}
+
+// Заполняем массив
+double[] GenArray(int arrLength, int maxValue)
+{
+    double[] array = new double[arrLength];
+    Random ren = new Random();
+
+    for (int i = 0; i < arrLength; i++)
+    {
+        array[i] = Math.Round((ren.NextDouble() * maxValue), 2);
+    }
+    return array;
+}
+
+// печатаем массив
+void PrintArray(double[] arr)
+{
+    Console.Write("[" + arr[0] + ", ");
+    for (int i = 1; i < arr.Length - 1; i++)
+    {
+        Console.Write(arr[i] + ", ");
+    }
+    Console.Write(arr[arr.Length - 1] + "]");
+    Console.WriteLine();
+}
+
+// разница между макс и мин
+double MaxMin(double[] arr)
+{
+    double min = arr[0];
+    double max = arr[0];
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] > max)
+            max = arr[i];
+        if (arr[i] < min)
+            min = arr[i];
+    }
+    return max - min;
+}
+
+double[] arr = GenArray(50, 100);
+
+PrintArray(arr);
+PrintResult("Разница между max и min = " + MaxMin(arr));
