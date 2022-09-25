858c7215d4d35e871415c82309fc92d805f7c415
Gasanov Nazim <nazimushka@gmail.com>
2022-09-25 23:51:07 +0300

    Результаты домашней работы №7

---
+++ Sem7Task47/Program.cs
@@ -0,0 +1,54 @@
+﻿// # 47 Задайте двумерный массив размером m×n,
+// заполненный случайными вещественными числами.
+// m = 3, n = 4.
+// 0,5 7 -2 -0,2
+// 1 -3,3 8 -9,9
+// 8 7,8 -7,1 9
+
+// Чтение данных из консоли
+int ReadData(string line)
+{
+    // Выводим сообщение
+    Console.WriteLine(line);
+    // Считываем число
+    int number = int.Parse(Console.ReadLine() ?? "0");
+    // Возвращаем значение
+    return number;
+}
+
+// Ззаполнение двумерного массива
+double[,] Fill2DArray(int row, int column, int downBorder, int topBorder)
+{
+    System.Random rand = new System.Random();
+    double[,] array2D = new double[row, column];
+
+    for (int i = 0; i < row; i++) //строка
+    {
+        for (int j = 0; j < column; j++) //столбец
+        {
+            array2D[i, j] = Math.Round(rand.Next(downBorder, topBorder) * rand.NextDouble(), 2);
+        }
+    }
+    return array2D;
+}
+
+// Печать двумерного массива
+void Print2DArray(double[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            Console.Write($"{matr[i, j]} \t ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int column = 3;
+int row = 4;
+int downBorder = -10;
+int topBorder = 10;
+
+double[,] arr2D = Fill2DArray(row, column, downBorder, topBorder);
+Print2DArray(arr2D);
---
+++ Sem7Task50/Program.cs
@@ -0,0 +1,87 @@
+﻿// # 50 Напишите программу, которая на вход принимает
+// позиции элемента в двумерном массиве, и
+// возвращает значение этого элемента или же указание,
+// что такого элемента нет.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 17 -> такого числа в массиве нет
+// * Заполнить числами Фиббоначи и выделить цветом найденную цифру
+
+// Чтение данных из консоли
+int ReadData(string line)
+{
+    // Выводим сообщение
+    Console.WriteLine(line);
+    // Считываем число
+    int number = int.Parse(Console.ReadLine() ?? "0");
+    // Возвращаем значение
+    return number;
+}
+
+// Метод генерации и заполнения массива
+int[,] Fill2DArray(int row, int column, int min, int max)
+{
+    System.Random rand = new System.Random();
+    int[,] array2D = new int[row, column];
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < column; j++)
+        {
+            array2D[i, j] = new Random().Next(min, max + 1);
+        }
+    }
+    return array2D;
+}
+
+// Печать двумерного массива
+void Print2DArray(int[,] array2D)
+{
+    for (int i = 0; i < array2D.GetLength(0); i++)
+    {
+        for (int j = 0; j < array2D.GetLength(1); j++)
+        {
+            Console.Write($"{array2D[i, j]} \t ");
+        }
+        Console.WriteLine();
+    }
+}
+
+//Поиск элемента координаты
+int FindElement(int x, int y, int[,] array2D)
+{
+    if (x < array2D.GetLength(0) && y < array2D.GetLength(1))
+    {
+        return array2D[x, y]; // x = строка; y = столбец
+    }
+    else
+    {
+        return -1;
+    }
+}
+
+// Печать результата
+void PrintResult(int x, int y, int[,] array2D)
+{
+    if (x < array2D.GetLength(0) && y < array2D.GetLength(1))
+    {
+        Console.WriteLine($"В этих координатах лежит число: {array2D[y, x]}");
+    }
+    else
+    {
+        Console.WriteLine("Элемент с координатами отсутствует");
+    }
+}
+
+int row = ReadData("Введите количество строк: m: ");
+int column = ReadData("Введите количество столбцов: ");
+Console.WriteLine();
+
+int[,] array = Fill2DArray(row, column, 1, 99);
+Print2DArray(array);
+Console.WriteLine();
+int x = ReadData("Введите координату строки m: ") - 1;
+int y = ReadData("Введите координату столбца n: ") - 1;
+int element = FindElement(y, x, array);
+PrintResult(y, x, array);
